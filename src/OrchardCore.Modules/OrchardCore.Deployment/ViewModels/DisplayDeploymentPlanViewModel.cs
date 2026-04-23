1053128d8a38ba4b8ea70778a5135c303b1300bb
appcompat-wx <appcompatwx@microsoft.com>
2026-04-23 15:57:58 +0800

    delete original code

--- src/OrchardCore.Modules/OrchardCore.Deployment/ViewModels/DisplayDeploymentPlanViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace OrchardCore.Deployment.ViewModels;
-
-public class DisplayDeploymentPlanViewModel
-{
-    public DeploymentPlan DeploymentPlan { get; set; }
-    public IEnumerable<dynamic> Items { get; set; }
-    public IDictionary<string, dynamic> Thumbnails { get; set; }
-}
--- src/OrchardCore/OrchardCore.Roles.Core/SystemRoleOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace OrchardCore.Roles;
-
-public class SystemRoleOptions
-{
-    public string SystemAdminRoleName { get; set; }
-}
